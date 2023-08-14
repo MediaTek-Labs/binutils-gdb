f82f3de328a4c12218a4b6163bffd92292841e7c
Faraz Shahbazker <faraz.shahbazker@mediatek.com>
2023-08-14 09:47:47 +0530

    Fix crash due to explicit relocations
    gas/
    	* config/tc-nanomips.c(reloc_sops): Remove BFD relocations
    	  that have no ELF counter-part.
    
    gas/
    	* testsuite/gas/nanomips/exp-relocs.d: Add test cases.
    	* testsuite/gas/nanomips/exp-relocs.d: Update reference output.

--- gas/testsuite/gas/nanomips/exp-relocs.s
+++ gas/testsuite/gas/nanomips/exp-relocs.s
@@ -47,6 +47,12 @@ test:
 1:	jalr	$t9
 	.reloc	1f,R_NANOMIPS_JALR16,end
 1:	jalr	$t9
+	.reloc	1f,R_NANOMIPS_INSN32,end
+1:	balc	foo
+	.reloc	1f,R_NANOMIPS_INSN16,foo
+1:	bc end
+	.reloc	1f,R_NANOMIPS_FIXED,foo
+1:	beqc $a3, $a4, end
 end:
 
 	addiu.w	$a0,$gp,%tlsgd(tlsvar)
